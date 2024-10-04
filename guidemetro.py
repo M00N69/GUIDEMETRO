da096e33d0d34820003acf69059ca15b1e730fdc
MOON <133333114+M00N69@users.noreply.github.com>
2024-10-04 08:23:57 +0200

    Update guidemetro.py

--- guidemetro.py
+++ guidemetro.py
@@ -5,6 +5,17 @@ from scipy import stats
 from scipy.interpolate import interp1d
 from io import BytesIO
 
+# Tableau global pour l'Annexe 4
+table_pom_x = [0.184, 0.175, 0.167, 0.161, 0.155, 0.149, 0.144, 0.140, 0.136, 0.132, 0.129, 0.126, 
+               0.123, 0.120, 0.118, 0.115, 0.113, 0.111, 0.109, 0.107, 0.105, 0.103, 0.102, 0.100, 
+               0.099, 0.097, 0.096, 0.095, 0.093, 0.092, 0.091, 0.090, 0.089, 0.088, 0.087, 0.086, 
+               0.085, 0.084, 0.083, 0.082, 0.081, 0.080, 0.079, 0.078, 0.077, 0.076, 0.075, 0.074, 
+               0.073, 0.072]  # Ajout des deux dernières valeurs extrapolées pour égaliser la longueur
+
+table_pom_y = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 
+               145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 
+               230, 235, 240, 245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 300]
+
 # Fonction pour le test de normalité de Shapiro-Wilk
 def shapiro_test(data):
     stat, p = stats.shapiro(data)
@@ -16,25 +27,15 @@ def shapiro_test(data):
 
 # Fonction pour calculer le POM à partir de l'annexe 3 du guide DGCCRF
 def calcul_pom(delta_sqrt_n):
-    table_pom_x = [0.17, 0.28, 0.39, 0.50, 0.61, 0.72, 0.83, 0.94, 1.06, 1.17, 1.28, 1.39, 1.50, 1.61, 1.72, 1.83, 1.94, 2.05, 2.17, 2.28, 2.39, 2.50, 2.61, 2.72, 2.83, 2.94, 3.05, 3.16, 3.27, 3.38]
-    table_pom_y = [328, 108, 66, 44, 30, 23, 18, 15, 12, 10, 9, 8, 7, 6, 6, 5, 5, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2]
-    f = interp1d(table_pom_x, table_pom_y, kind='linear', fill_value="extrapolate")
+    table_pom_x_annex3 = [0.17, 0.28, 0.39, 0.50, 0.61, 0.72, 0.83, 0.94, 1.06, 1.17, 1.28, 1.39, 1.50, 1.61, 1.72, 1.83, 1.94, 2.05, 2.17, 2.28, 2.39, 2.50, 2.61, 2.72, 2.83, 2.94, 3.05, 3.16, 3.27, 3.38]
+    table_pom_y_annex3 = [328, 108, 66, 44, 30, 23, 18, 15, 12, 10, 9, 8, 7, 6, 6, 5, 5, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2]
+    f = interp1d(table_pom_x_annex3, table_pom_y_annex3, kind='linear', fill_value="extrapolate")
     if isinstance(delta_sqrt_n, (np.ndarray, np.float64)):
         delta_sqrt_n = float(delta_sqrt_n)
     return round(float(f(delta_sqrt_n)))
 
 # Fonction pour calculer le POM pour les défauts
 def calcul_pom_defectueux(delta_sqrt_n):
-    table_pom_x = [0.184, 0.175, 0.167, 0.161, 0.155, 0.149, 0.144, 0.140, 0.136, 0.132, 0.129, 0.126, 
-                   0.123, 0.120, 0.118, 0.115, 0.113, 0.111, 0.109, 0.107, 0.105, 0.103, 0.102, 0.100, 
-                   0.099, 0.097, 0.096, 0.095, 0.093, 0.092, 0.091, 0.090, 0.089, 0.088, 0.087, 0.086, 
-                   0.085, 0.084, 0.083, 0.082, 0.081, 0.080, 0.079, 0.078, 0.077, 0.076, 0.075, 0.074, 
-                   0.073, 0.072]  # Ajout des deux dernières valeurs extrapolées pour égaliser la longueur
-    
-    table_pom_y = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 
-                   145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 
-                   230, 235, 240, 245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 300]
-    
     # Vérification stricte des longueurs des tableaux
     if len(table_pom_x) != len(table_pom_y):
         st.write(f"Erreur: Longueur de table_pom_x = {len(table_pom_x)}, Longueur de table_pom_y = {len(table_pom_y)}")
@@ -205,4 +206,5 @@ if uploaded_file is not None:
 
 else:
     st.info("Veuillez télécharger un fichier Excel pour commencer l'analyse.")
+
 
